0a9043ff83abc7d7e03a33cd5a78663262940caf
Ryo Hashiguchi <ryo12redstone@gmail.com>
2016-08-17 17:14:36 +0900

    Add Finished.js

---
+++ participant/Finished.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+const mapStateToProps = ({}) => ({
+})
+
+const Experiment = ({}) => (
+  <div>
+    <p>実験終了画面</p>
+  </div>
+)
+
+export default connect(mapStateToProps)(Experiment)
