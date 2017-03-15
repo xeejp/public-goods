559859d075914d7cbc93dbc6a8fbc59e044b31a5
Ryohei HAYASHI <hayashi@xee.jp>
2017-03-16 00:18:55 +0900

    Change description to add variables.

--- participant/Description.js
+++ participant/Description.js
@@ -4,27 +4,25 @@ import { connect } from 'react-redux'
 import {Card, CardText, CardTitle } from 'material-ui/Card'
 import {List, ListItem} from 'material-ui/List'
 
-const mapStateToProps = ({}) => ({
-})
+const mapStateToProps = ({ rounds, roi, money }) => ({ rounds, roi, money })
 
-const Description = ({}) => (
-  <Card>
-    <CardTitle title="公共財実験" subtitle="ルールの説明" />
-    <CardText>
-      <p>あなたは他の3名の参加者とともに4人1組のグループに属しています。<br />このグループのメンバーは実験を通じて変わりません。</p>
-      <p>グループのメンバーには各ターンごとに100ポイントずつ与えられます。<br />あなたはこの100ポイントを、私的財と公共財に振り分けて投資してください。</p>
-      <List>
-        <ListItem
-          primaryText="私的財"
-          secondaryText="投資したポイントがそのまま自分の収益になります。"
-        />
-        <ListItem
-          primaryText="公共財"
-          secondaryText="メンバー全員の投資したポイントを合計して、0.4を乗じたポイントが全員の収益になります。"
-        />
-      </List>
-      <p>各ターンの収益は各ターンごとに集計され、次のターンには持ち越されません。<br />つまり、どのターンでも投資できるポイントは100ポイントです。<br />収益は累積され、画面右上に表示されます。</p>
-    </CardText>
-  </Card>
-)
-export default connect()(Description)
+class Description extends Component {
+  render() {
+    const { rounds, roi, money } = this.props
+    return (
+      <Card>
+        <CardTitle title="公共財実験" subtitle="ルールの説明" />
+        <CardText>
+          <p>あなたは他の3名の参加者とともに4人1組のグループに属しています。<br />このグループのメンバーは実験を通じて変わりません。</p>
+          <p>グループのメンバーには各ターンごとに{ money }ポイントずつ与えられます。<br />あなたはこの{ money }ポイントを、私的財と公共財に振り分けて投資してください。</p>
+          <p>私的財：投資したポイントがそのまま自分の収益になります。</p>
+          <p>公共財：メンバー全員の投資したポイントを合計して、{ roi }を乗じたポイントが全員の収益になります。</p>
+          <p>各ターンの収益は各ターンごとに集計され、次のターンには持ち越されません。<br />つまり、どのターンでも投資できるポイントは{ money }ポイントです。<br />収益は累積され、画面右上に表示されます。</p>
+          <p>この投資を全部で{ rounds }ターン繰り返します。</p>
+        </CardText>
+      </Card>
+    )
+  }
+}
+
+export default connect(mapStateToProps)(Description)
