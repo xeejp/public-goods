aa3ddc1ef7440b4639e3760f464750ebd96585a8
unknown <hayashi@xee.jp>
2018-08-31 22:52:18 +0900

    working directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,32 @@
+const path = require('path');
+require("@babel/polyfill");
+
 module.exports = {
-  devtool: 'eval-source-map',
+  mode: "production",
   entry: {
-    host: ["babel-polyfill", "./host/index.js"],
-    participant: ["babel-polyfill", "./participant/index.js"],
+    host: ["@babel/polyfill", "./host/index"],
+    participant: ["@babel/polyfill", "./participant/index"],
   },
   output: {
-    path: "./",
-    filename: "[name].js"
+    path: path.resolve(__dirname, "./"),
+    filename: "[name].js",
+    publicPath: "",
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: "babel"
-    }]
+    rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        },
+      }
+    ]
   },
   resolve: {
-    extensions: [
-      "", ".js"
-    ],
-    modulesDirectories: [
-      "node_modules", "./"
-    ]
-  }
-};
+     extensions: ['.js', '.jsx'],
+   },
+}
